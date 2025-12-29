17d63a8e5cedad9b2ce70b6ea51428b1062a87a4
malua <malua@users.noreply.github.com>
2025-12-29 22:51:39 +0100

    refactor: put frontend url in dev vars

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,18 +5,19 @@ import { dbMiddleware } from "@backend/services/db";
 import { authenticationMiddleware } from "@backend/services/auth/authentication-middleware";
 import { authorizationMiddleware } from "@backend/services/auth/authorization-middleware";
 import { cors } from "hono/cors";
+import { env } from "hono/adapter";
 
 const app = factory
   .createApp()
   .use(dbMiddleware)
-  .use(
-    "/api/*",
-    cors({
-      origin: ["https://tools.malua.dev"],
+  .use("/api/*", (c, next) => {
+    const { FRONTEND_URL } = env<{ FRONTEND_URL: string }>(c);
+    return cors({
+      origin: FRONTEND_URL,
       credentials: true,
       allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    }),
-  )
+    })(c, next);
+  })
   .use(authenticationMiddleware)
   .use(authorizationMiddleware)
   .route("/api", router)
